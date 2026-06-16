b0cd5e9707e94583849616660182a72173e61542
S4turn <91470154+Hyylassx@users.noreply.github.com>
2026-06-16 23:30:02 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -4,8 +4,7 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/recovery/root/ueventd.mt6765.rc:recovery/root/ueventd.mt6765.rc \
     $(DEVICE_PATH)/recovery/root/init.recovery.usb.rc:recovery/root/init.recovery.usb.rc \
     $(DEVICE_PATH)/recovery/root/init.recovery.mt6765.rc:recovery/root/init.recovery.mt6765.rc \
-    $(DEVICE_PATH)/recovery/root/init.recovery.microtrust.rc:recovery/root/init.recovery.microtrust.rc \ 
-    $(DEVICE_PATH)/foxstart.sh:root/sbin/foxstart.sh
+    $(DEVICE_PATH)/recovery/root/init.recovery.microtrust.rc:recovery/root/init.recovery.microtrust.rc
 
 # --- PART 2: SYSTEM ETC ---
 PRODUCT_COPY_FILES += \
