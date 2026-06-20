baf86063b07561202f6da505e12f6475aee1a2be
S4turn <91470154+Hyylassx@users.noreply.github.com>
2026-06-21 05:46:22 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,3 +1,13 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 The TWRP Open Source Project
+#
+
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE), X655C)
@@ -7,30 +17,33 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 include $(CLEAR_VARS)
 LOCAL_MODULE := vold_recovery
 LOCAL_MODULE_STEM := vold
-LOCAL_SRC_FILES := prebuilt/vold
+LOCAL_SRC_FILES := vold
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/system/bin
+LOCAL_CHECK_ELF_FILES := false
 include $(BUILD_PREBUILT)
 
 # Inject Prebuilt keystore
 include $(CLEAR_VARS)
 LOCAL_MODULE := keystore_recovery
 LOCAL_MODULE_STEM := keystore
-LOCAL_SRC_FILES := prebuilt/keystore
+LOCAL_SRC_FILES := keystore
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/system/bin
+LOCAL_CHECK_ELF_FILES := false
 include $(BUILD_PREBUILT)
 
 # Inject Prebuilt gatekeeperd
 include $(CLEAR_VARS)
 LOCAL_MODULE := gatekeeperd_recovery
 LOCAL_MODULE_STEM := gatekeeperd
-LOCAL_SRC_FILES := prebuilt/gatekeeperd
+LOCAL_SRC_FILES := gatekeeperd
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/system/bin
+LOCAL_CHECK_ELF_FILES := false
 include $(BUILD_PREBUILT)
 
 endif
