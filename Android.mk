f2aebf071dbc56db66e8b7ced8ddeda0ba3fa500
S4turn <91470154+Hyylassx@users.noreply.github.com>
2026-06-21 04:48:20 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -12,4 +12,35 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE), X655C)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+# Inject Prebuilt vold
+include $(CLEAR_VARS)
+LOCAL_MODULE := vold_recovery
+LOCAL_MODULE_STEM := vold
+LOCAL_SRC_FILES := vold  # (Change to prebuilt/vold if they are in a folder)
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/system/bin
+include $(BUILD_PREBUILT)
+
+# Inject Prebuilt keystore
+include $(CLEAR_VARS)
+LOCAL_MODULE := keystore_recovery
+LOCAL_MODULE_STEM := keystore
+LOCAL_SRC_FILES := keystore  # (Change to prebuilt/keystore if they are in a folder)
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/system/bin
+include $(BUILD_PREBUILT)
+
+# Inject Prebuilt gatekeeperd
+include $(CLEAR_VARS)
+LOCAL_MODULE := gatekeeperd_recovery
+LOCAL_MODULE_STEM := gatekeeperd
+LOCAL_SRC_FILES := gatekeeperd  # (Change to prebuilt/gatekeeperd if they are in a folder)
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/system/bin
+include $(BUILD_PREBUILT)
+
 endif
